0f85021ace3dcd15f3573e95ae94fbfcc186fc0b
Oluwatobiloba Amu <60395243+amuabigail@users.noreply.github.com>
2021-09-03 14:36:09 -0700

    created the game section

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// import HomePage from "./Pages/Homepage.js/Homepage";
+import Games from "./components/Games/game.js";
+
+
+function App() {
+  return (
+    // <div className="App">
+    //  <HomePage />
+    //  </div>
+     <div className="App">
+     <Games />
+     </div>
+  );
+}
+
+export default App;
